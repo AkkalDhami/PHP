7ec2411862f77cadba1be519cd400c4d89576738
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-15 20:14:41 +0545

    display user details

---
+++ Practise-Qsn/lab/g_page.php
@@ -0,0 +1,12 @@
+<?php
+    $id = $_POST['id'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+
+    echo "ID: $id<br>";
+    echo "Name: $name<br>";
+    echo "Email: $email<br>";
+    echo "Password: $password<br>";
+    echo "<br>Program Executed By Akkal Dhami";
+?>
---
+++ Practise-Qsn/lab/g_user-details.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./g_page.php" method="post">
+        <label>ID:</label><br>
+        <input type="text" name="id"> <br>
+        <label>Name:</label><br>
+        <input type="text" name="name"> <br>
+        <label>Email:</label><br>
+        <input type="email" name="email"> <br>
+        <label>Password:</label><br>
+        <input type="password" name="password"> <br>
+        <input type="submit" value="Submit"> <br>
+    </form>
+</body>
+
+</html>
