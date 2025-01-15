532d758ed944940a124a89f3f4e2e9a86f409955
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-15 20:13:41 +0545

    find multiplication table of 23

---
+++ Practise-Qsn/lab/d_multiplicationtable_of_23.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+</body>
+</html>
+<?php
+
+    echo "Multiplication Table of 23 <br>";
+    for ($i=1; $i<=10  ; $i++) { 
+        echo "23 * {$i} = ".(23*$i)."<br>";
+    }
+    echo "Program Executed By Akkal Dhami";
+?>
