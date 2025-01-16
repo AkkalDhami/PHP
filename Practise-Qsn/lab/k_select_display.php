68a806a35120d53719894fbfd9ea68884d2ea8d4
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-16 12:16:05 +0545

    select user and display

---
+++ Practise-Qsn/lab/k_select_display.php
@@ -0,0 +1,37 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_user";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, name, email FROM user";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <td>{$row['id']}</td>
+                <td>{$row['name']}</td>
+                <td>{$row['email']}</td>
+              </tr>";
+    }
+    echo "</table>";
+    echo "<br>Program Executed By Akkal Dhami";
+} else {
+    echo "No records found.";
+}
+
+mysqli_close($conn);
+?>
