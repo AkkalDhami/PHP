dd7f30c0cee8d7f608dfef7129aa06ced021956f
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-24 15:34:01 +0545

    update code

--- Practise-Qsn/lab/j_insert_five_records.php
+++ Practise-Qsn/lab/j_insert_five_records.php
@@ -9,18 +9,45 @@ $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-    $id = 115;  
-    $name = "akkal2";
-    $email = "akkal2@gmail.com";
-    $password = "akkal123"; 
 
-    $sql = "INSERT INTO user  VALUES ($id, '$name', '$email', '$password')";
+
+    $sql = "INSERT INTO user  VALUES (
+        1,
+        'rajan',
+        'arajan@gmail.com',
+        'rajan123'
+    ),
+    (
+        2,
+        'rajan',
+        'rajan2@gmail.com',
+        'rajan123sad'
+    ),
+    (
+        3,
+        'rajan4',
+        'rajan4@gmail.com',
+        'rajanas123'
+    ),
+    (
+        4,
+        'rajan5',
+        'rajan5@gmail.com',
+        'raasdjan123'
+    ),
+    (
+        5,
+        'rajan6',
+        'rajan6@gmail.com',
+        'rajasadn123'
+    )";
 
     if (mysqli_query($conn, $sql)) {
-        echo "Record for $name inserted successfully.<br>";
+        echo "Record for 5 users inserted successfully.<br>";
         echo "Program Executed By Akkal Dhami";
+     
     } else {
-        echo "Error inserting record for $name: " . mysqli_error($conn) . "<br>";
+        echo "Error inserting record for 5 users : " . mysqli_error($conn) . "<br>";
     }
 
 mysqli_close($conn);
